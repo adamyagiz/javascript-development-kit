5e6ea6b149710df6fc84203ec72f306f2c00ed40
Adam Yagiz <akyagiz@gmail.com>
2017-09-15 14:18:31 -0700

    added some comments to test file

--- src/index.test.js
+++ src/index.test.js
@@ -4,12 +4,14 @@ import {expect} from 'chai'; // using the "expect" style that comes with Chai
 import jsdom from 'jsdom';
 import fs from 'fs';
 
+// dummy test
 describe('Our first test', () => {
   it('should pass', () => {
     expect(true).to.equal(true);
   });
 });
 
+// check the inner html of the h1 tag for specific value
 describe('index.html', () => {
   it('should say hello', (done) => {
     const index = fs.readFileSync('./src/index.html', 'utf-8');
